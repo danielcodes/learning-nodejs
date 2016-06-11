1203b7d52eb23ecaec3a1a9a25eab3e2b04a7255
Daniel Chia <daniel_1118@hotmail.com>
2016-06-11 11:58:32 -0400

    init commit, setting up express

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+//instance of express
+var app = express();
+
+var port = 5000;
+
+app.get('/', function(req, res){
+	res.send('hello world');
+
+});
+
+app.get('/books', function(req, res){
+	res.send('hello books');
+
+});
+
+app.listen(port, function(err){
+	console.log('running server on port ' + port);
+
+});
