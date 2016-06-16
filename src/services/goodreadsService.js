6312442d712c3aa82c1b2fbde610248ca0875895
Daniel Chia <daniel_1118@hotmail.com>
2016-06-16 13:55:51 -0400

    obtained book description and image by hitting goodreads API

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,8 +1,37 @@
+var http = require('http');
+var xml2js = require('xml2js');
+var parser = xml2js.Parser({explicitArray: false});
+
 var goodreadsService = function(){
 
 	var getBookById = function(id, cb){
-		cb(null, {description: "Some description, damn this is dense"});
 
+		var options = {
+			host: 'www.goodreads.com',
+			path: '/book/show/656?format=xml&key=IFgIxvEJWsv3bun9ncp4wg'
+		};
+
+		var callback = function(response){
+			var str = '';
+
+			response.on('data', function(chunk){
+				str += chunk;	
+			});	
+
+			response.on('end', function(){
+				console.log(str);
+				parser.parseString(str, function(err, result){
+
+					console.log('the result is ', result);
+					//call the callback now
+					cb(null, result.GoodreadsResponse.book);
+				});
+					
+			});
+		};
+
+
+		http.request(options, callback).end();
 
 	};
 
